781e6d85d151e22a11d41fbc4a1013e7c22f4783
Jack Lambert <jackwlambert14@gmail.com>
2022-10-25 11:41:41 -0400

    init commit fizzbuzz practice

---
+++ program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+void FizzBuzz(int m) {
+
+    for(int i =1; i <= m; i++) {
+        if(i % 3 == 0 && i % 5 == 0){
+        System.Console.WriteLine("FizzBuzz");}
+        else if(i % 3 == 0 ){
+        System.Console.WriteLine("Fizz");}
+        else if(i % 5 == 0){
+        System.Console.WriteLine("Buzz");}
+        else {
+            System.Console.WriteLine($"{i}");
+        }
+    }
+}
+
+FizzBuzz(150);
